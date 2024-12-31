9ad2a5b1e9fe0785d7533447f2a3b989ff648b78
Andre <andrevitorlopes@gmail.com>
2024-12-31 14:22:19 +0100

    Add first lambda

---
+++ iac/lambdas.tf
@@ -0,0 +1,56 @@
+locals {
+  process_data_lambda_name = "process-data"
+}
+
+resource "aws_iam_role" "process_data" {
+  name               = "${local.process_data_lambda_name}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy" "process_data_policies" {
+  role   = aws_iam_role.process_data.name
+  policy = data.aws_iam_policy_document.process_data_policies.json
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = data.archive_file.lambda.output_path
+  function_name = local.process_data_lambda_name
+  role          = aws_iam_role.process_data.arn
+  handler       = "index.handler"
+  runtime       = "dotnet8"
+}
+
+data "archive_file" "dotnet_lambda_archive" {
+  type        = "zip"
+  source_dir  = "dotnet_lambda_default"
+  output_path = "dotnet_lambda_function_payload.zip"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+
+  }
+}
+
+data "aws_iam_policy_document" "process_data_policies" {
+  statement {
+    effect = "Allow"
+    sid    = "LogToCloudwatch"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
