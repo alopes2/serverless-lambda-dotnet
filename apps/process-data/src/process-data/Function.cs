b8fe2235176e17d995a39618d63fcf11dad53d38
Andre <andrevitorlopes@gmail.com>
2024-12-31 14:46:46 +0100

    Update lambda code and handler

--- apps/process-data/src/process-data/Function.cs
+++ apps/process-data/src/process-data/Function.cs
@@ -3,11 +3,11 @@ using Amazon.Lambda.Core;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace process_data;
+namespace Lambda;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
